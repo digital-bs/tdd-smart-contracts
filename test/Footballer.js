f3bb67c032161e3420e147b772dfddc569462de1
digi <Semen7empty@gmail.com>
2023-05-22 01:16:51 +0700

    add test for const dnaDigits in contract

--- test/Footballer.js
+++ test/Footballer.js
@@ -2,9 +2,22 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("FootballerAcademy", function(){
+    let footballerContractFactory;
+    let footballerContract;
 
     it("get Footballer contract", async function(){
         const footballerContract = await ethers.getContractFactory("FootballerFactory");
     });
+    beforeEach(async function() {        
+        footballerContractFactory = await ethers.getContractFactory("FootballerFactory");
+        footballerContract = await footballerContractFactory.deploy();
+    })
+
+    it("get dnaDigits", async function() {        
+        const dnaDigits = await footballerContract.getDnaDigits();
+        expect(dnaDigits).to.equal(16);
+    });
+
+
 
 });
